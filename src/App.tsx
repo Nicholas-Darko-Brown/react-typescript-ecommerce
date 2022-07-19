e3aa518f492030f377d8324d9ccc19276d3f3f4e
Nicholas-Darko-Brown <nicholas.brown@amalitech.com>
2022-07-19 13:37:22 +0000

    feat: created routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import { ShoppingCartProvider } from './context/ShoppingCartContext';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Store from './pages/Store';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ShoppingCartProvider>
+      <Router>
+        <Header />
+          <Routes>
+            <Route path='/store' element={<Store />}></Route>
+            <Route path='/contact' element={<Contact />}></Route>
+            <Route path='/' element={<Home />}></Route>
+          </Routes>
+      </Router>
+    </ShoppingCartProvider>
   );
 }
 
