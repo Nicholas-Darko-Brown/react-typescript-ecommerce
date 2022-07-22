c8591b50e354451ab8a74e1070ac74303fed786f
Nicholas-Darko-Brown <nicholas.brown@amalitech.com>
2022-07-22 14:33:53 +0000

    feat: added footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "../../styles/Footer.css"
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <div className="footer1">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, animi.
+      </div>
+      <div className="footer2">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi, ipsam?
+      </div>
+      <div className="footer3">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit, cupiditate!
+      </div>
+    </div>
+  )
+}
+
+export default Footer
